80dd83b6d9d3199326f538991e70a90f56f2b994
Dudix93 <dodo45693@gmail.com>
2020-03-06 22:20:16 +0000

    Cards grid with titles

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -8,14 +8,19 @@ class APIController extends Controller
 {
     public function index() {
         $page_name = 'dodo123';
+        // $url = 'https://jsonplaceholder.typicode.com/todos';
+        $url = 'https://api.jikan.moe/v3/season/2020/winter';
         $client = new \GuzzleHttp\Client([
             'verify' => false,
         ]);
-        $res = $client->request('GET', 'https://api.jikan.moe/v3/anime/200', [
+        $res = $client->request('GET', $url, [
             'content-type' => 'application/vnd.api+json',
-            // 'access' => 'application/vnd.api+json'
+            'access' => 'application/vnd.api+json'
         ]);
-        dd($res->getBody()->getContents());
-        return view('welcome', ['page_name' => $page_name]);
+        // dd(json_decode($res->getBody()->getContents()));
+        return view('welcome', [
+            'page_name' => $page_name,
+            'titles' => json_decode($res->getBody()->getContents())
+        ]);
     }
 }
---
+++ resources/views/card.blade.php
@@ -0,0 +1,9 @@
+{{-- @extends('layouts.app') --}}
+<div class="card" style="width: 18rem; margin:10px">
+    <img class="card-img-top" style="height: 300px" src = {{ $anime->image_url}} alt = {{ $anime->title }}>
+        <div class="card-body">
+            <h5 class="card-title">{{ $anime->title }}</h5>
+            {{-- <p class="card-text">Description.</p> --}}
+            {{-- <a href="#" class="btn btn-primary">Go somewhere</a> --}}
+        </div>
+</div>
